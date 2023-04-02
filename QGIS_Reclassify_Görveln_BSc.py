f26f589d3d5b6727062caa58eb48f1a3cdaff1b6
Lozon95 <129620605+Lozon95@users.noreply.github.com>
2023-04-02 21:56:56 +0200

    Add files via upload

---
+++ QGIS_Reclassify_Görveln_BSc.py
@@ -0,0 +1,49 @@
+from osgeo import gdal 
+import numpy as np
+
+# Set input raster file name
+raster_file = "TIN_Görveln_PT1.tif"
+
+# Open raster dataset and read data
+ds = gdal.Open(raster_file)
+data = ds.GetRasterBand(1).ReadAsArray()
+print(data.shape) 
+
+print("Open raster dataset")
+
+# Create a new GeoTIFF driver
+driver_gtiff = gdal.GetDriverByName('GTiff')
+
+print("Driver has been opened")
+
+# Set output raster file name
+fn_copy = "/mnt/c/Users/andbo/Desktop/Tin_Görveln_PT1_copy2.tif"
+
+# Create a copy of the input raster
+ds_copy = driver_gtiff.CreateCopy(fn_copy, ds)
+
+print("Copy has been created")
+
+# Define the reclassification function
+def reclass(array):
+    new_array = np.zeros(array.shape, dtype=np.float32)
+    new_array[array == -9999] = np.nan
+    new_array[array >= -11.5] = 0
+    new_array[array <= -40] = 28.5
+    new_array[(array > -40) & (array < -11.5)] = abs(array[(array > -40) & (array < -11.5)] + 11.5)
+    return new_array
+
+print("Starting reclass")
+# Reclassify the input data
+new_raster = reclass(data)
+print("Reclass finished")
+print("Starting to write reclassified data")
+# Write the reclassified data to the output raster
+ds_copy.GetRasterBand(1).WriteArray(np.array(new_raster))
+ds_copy.FlushCache()
+print("data written, closing")
+# Close the raster datasets
+ds = None
+ds_copy = None
+print("closed")
+iface.addRasterLayer(ds_copy, "Görveln_omklassificerad")
---
+++ QGIS_Reclassify_Skarven_BSc.py
@@ -0,0 +1,48 @@
+from osgeo import gdal 
+import numpy as np
+
+# Set input raster file name
+raster_file = "TIN_Skarven_PT1.tif"
+
+# Open raster dataset and read data
+ds = gdal.Open(raster_file)
+data = ds.GetRasterBand(1).ReadAsArray()
+print(data.shape) 
+
+print("Open raster dataset")
+
+# Create a new GeoTIFF driver
+driver_gtiff = gdal.GetDriverByName('GTiff')
+
+print("Driver has been opened")
+
+# Set output raster file name
+fn_copy = "C:/Users/ander/Documents/Tin_Skarven_PT1_copy2.tif"
+
+# Create a copy of the input raster
+ds_copy = driver_gtiff.CreateCopy(fn_copy, ds)
+
+print("Copy has been created")
+
+# Define the reclassification function
+def reclass(array):
+    new_array = np.zeros(array.shape, dtype=np.float32)
+    new_array[array == -9999] = np.nan
+    new_array[array >= -9.4] = 0
+    new_array[array <= -11.9] = 2.5
+    new_array[(array > -11.9) & (array < -9.4)] = abs(array[(array > -11.9) & (array < -9.4)] + 9.4)
+    return new_array
+
+print("Starting reclass")
+# Reclassify the input data
+new_raster = reclass(data)
+print("Reclass finished")
+print("Starting to write reclassified data")
+# Write the reclassified data to the output raster
+ds_copy.GetRasterBand(1).WriteArray(np.array(new_raster))
+ds_copy.FlushCache()
+print("data written, closing")
+# Close the raster datasets
+ds = None
+ds_copy = None
+print("closed")
